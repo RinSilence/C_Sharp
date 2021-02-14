6d896d655af8e6301ce84ba2d442c9883717fce3
RinSilence <79023543+RinSilence@users.noreply.github.com>
2021-02-14 10:16:32 +0300

    Add files via upload

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now.ToShortDateString()}");
+
+            Console.ReadLine();
+        }
+    }
+}
